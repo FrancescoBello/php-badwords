91b93e9b67d63322dd3aec6ec58df116f2c92023
Francesco <fraancesco.bello@gmail.com>
2020-12-14 17:39:22 +0100

    sostituita badword con ***

--- index.php
+++ index.php
@@ -7,12 +7,29 @@
   <body>
 
       <?php
+      $badword = $_GET[$replace];
         $paragrafo = "Lorem ipsum dolor sit amet, consectetur adipisicing elit,
          sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Ut enim ad minim veniam,
           quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
            Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-           Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. "
-            echo "$paragrafo";  ?>
+           Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. ";
+
+            $replace = str_replace('ipsum', '***', $paragrafo);
+
+            echo "$replace";
+
+
+
+
+
+
+
+
+
+
+
+
+            ?>
 
 
 
